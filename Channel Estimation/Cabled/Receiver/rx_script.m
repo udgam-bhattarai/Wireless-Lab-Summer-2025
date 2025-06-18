1b847b0c2c00c3ef57163c1e60313a90a6cedf36
udgam-bhattarai <157351385+udgam-bhattarai@users.noreply.github.com>
2025-06-18 11:33:10 +0400

    Update rx_script.m

--- Channel Estimation/Cabled/Receiver/rx_script.m
+++ Channel Estimation/Cabled/Receiver/rx_script.m
@@ -1,99 +1,89 @@
 clc;
 clear;
 
-
 cfgNonHT = wlanNonHTConfig(PSDULength=61);
 
 %% Packet Detection
+rfRxFreq = 2.437e9; % Center frequency
+packetsDetected = 0; % Number of packets detected
 
-rfRxFreq = 2.437e9; %% Center frequency
-
-rx = comm.SDRuReceiver( ... %%must match transmitter side, except gain
-    'Platform',         'B210', ...
-    'SerialNum',        '344C4DE', ... %%can be found by runnin findsdru in terminal
+% must match transmitter side, except gain
+rx = comm.SDRuReceiver( ...
+    'Platform', 'B210', ...
+    'SerialNum', '344C4DE', ... % can be found by running findsdru in terminal
     'MasterClockRate', 20e6, ...
-    'CenterFrequency',  rfRxFreq,...
-    'Gain', 60,...
-    'OutputDataType', 'double',...
+    'CenterFrequency', rfRxFreq, ...
+    'Gain', 60, ...
+    'OutputDataType', 'double', ...
     'DecimationFactor', 1);
-%%
-%
 
+%% Samples Collection
 Nrx = 5000;
 
+% For each sample
 for i = 1:Nrx
-
-    disp(i/Nrx)
+    disp(i / Nrx)
     [rxData, ~] = capture(rx, 0.001, 'Seconds');
 
-    %%
-    % detect the packet
-
+    % Detect the packet
     try
-    startOffset1 = wlanPacketDetect(rxData, 'CBW20',0,0.25); % roughly detects how many samples away a preamble is from the start of the data
+        startOffset1 = wlanPacketDetect(rxData, 'CBW20', 0, 0.25);
 
+        % No packet detected (rudimentary). If high, likely no packet detected
+        if ~isempty(startOffset1)
+            % Calculate the LLTF index points
+            idxLLTF = wlanFieldIndices(cfgNonHT, 'L-LTF');
 
-    if ~isempty(startOffset1)%no packet detected (rudimentary). If high, likely no packet detected
-        
-        %%
-        % Calculate the LLTF index points
+            % Get non-HT fields
+            ind = wlanFieldIndices(cfgNonHT);
+            nonHTFields = rxData(startOffset1 + (ind.LSTF(1):ind.LSIG(2)), :);
+            startOffset2 = wlanSymbolTimingEstimate(nonHTFields, "CBW20"); % Finer preamble detection
 
-        idxLLTF = wlanFieldIndices(cfgNonHT, 'L-LTF');
-        %%
-        % Get non-HT fields
+            if startOffset2 > 1
+                packetsDetected = packetsDetected + 1;
+                % If startOffset2 is negative, packet detection likely failed
+                nonHTFields = rxData(startOffset1 + startOffset2 + (ind.LSTF(1):ind.LSIG(2)), :);
 
-        ind = wlanFieldIndices(cfgNonHT);
-        nonHTFields = rxData(startOffset1+(ind.LSTF(1):ind.LSIG(2)),:);
-        startOffset2 = wlanSymbolTimingEstimate(nonHTFields, "CBW20"); % fine preamble detection
+                % Demodulate the LLTF
+                demodSig = wlanLLTFDemodulate(nonHTFields(idxLLTF(1):idxLLTF(2), :), cfgNonHT);
 
-        if startOffset2>1 %if startoffset2 is negative packetdetection likely failed
-        nonHTFields = rxData(startOffset1+startOffset2+(ind.LSTF(1):ind.LSIG(2)),:);
-        %%
-        % Demodulate the LLTF
+                % Get channel estimation
+                subcarrier_index = [(6:31) (33:58)] + 1; % Subcarrier 32 is DC 0 in Wifi
+                H_hat = zeros(64, 1);
+                H_hat(subcarrier_index) = wlanLLTFChannelEstimate(demodSig, cfgNonHT); % Channel Estimation LTF
 
-        demodSig = wlanLLTFDemodulate(nonHTFields(idxLLTF(1):idxLLTF(2), :), cfgNonHT);
-        %%
-        % Get channel estimation
+                figure(1)
+                subplot(1, 2, 1)
+                plot(10 * log10(abs(H_hat).^2), '-', 'DisplayName', 'Estimated Channel, abs');
+                ylim([-40 0])
 
-        subcarrier_index = [(6:31) (33:58)] + 1; %subcarrier 32 is DC 0 in Wifi
-        H_hat = zeros(64,1);
-        H_hat(subcarrier_index) = wlanLLTFChannelEstimate(demodSig, cfgNonHT); % channel estimation LTF
+                h_hat = ifft(fftshift(H_hat));
+                subplot(1, 2, 2)
+                plot(abs(h_hat));
+                ylim([0 0.15])
+                drawnow;
 
-        figure(1)
-        subplot(1,2,1)
-        plot(10*log10(abs(H_hat).^2),'-','DisplayName','Estimated Channel, abs');
- 
-        ylim([-40 0])
-
-        h_hat = ifft(fftshift(H_hat));
-        subplot(1,2,2)
-
-        plot(abs(h_hat));
-        ylim([0 0.15])
-        drawnow;
-        % performance metrics
-
-        estimated_delay = startOffset1 + startOffset2;
-        % disp(['True delay: ' num2str(delay)])
-        disp(['Estimated delay: ' num2str(estimated_delay)]);
-        pause(0.01)
-        disp(['packet detected: ' num2str(i)])
+                % Performance metrics
+                estimated_delay = startOffset1 + startOffset2;
+                disp(['Estimated delay: ' num2str(estimated_delay)]);
+                pause(0.01)
+                disp(['Packet detected: ' num2str(i)])
+            else
+                disp(['Packet not detected ' num2str(i)])
+                pause(0.001)
+            end
         else
-            disp(['packed not detected ' num2str(i)])
+            disp(['Packet not detected ' num2str(i)])
             pause(0.001)
         end
-
-    else
-        disp(['packed not detected ' num2str(i)])
-        pause(0.001)
-    end
     catch
-        disp(['packed not detected ' num2str(i)])
+        disp(['Packet not detected ' num2str(i)])
         pause(0.001)
     end
-
 end
 
 release(rx);
 
-% mse = mean(abs(H_hat-H(subcarrier_index)).^2)
+%% Probability of Detection
+disp(['Probability of Packet Detection:', num2str(packetsDetected/Nrx)])
+
