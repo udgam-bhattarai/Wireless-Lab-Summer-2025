6cc1a7ad104830dd51c2e4110e3ebf57f1a9b67b
AndrewS <127096469+OrangeAah@users.noreply.github.com>
2025-07-29 14:54:35 +0530

    Added file used to parse data

---
+++ Channel Estimation/Simulation/parsedata.m
@@ -0,0 +1,25 @@
+data = load('csi_2023_10_16_1.txt');
+packetsize = 268;
+activeindeces = [7:32, 34:59];
+packet = data(1:packetsize);
+timestamp = packet(1:3);        % [hour, min, sec]
+rssi = packet(4:7);            % 4 RSSI values
+mcs = packet(8);               % Single MCS value
+gain = packet(9:12);           % 4 gain values
+csi_raw = packet(13:268); % 256 CSI values
+
+antenna1 = csi_raw(1:64);
+antenna1 = antenna1(activeindeces);
+
+antenna2= csi_raw(65:128);
+antenna2 = antenna2(activeindeces);
+
+antenna3 = csi_raw(129:192);
+antenna3 = antenna3(activeindeces);
+
+antenna4 = csi_raw(193:256);
+antenna4 = antenna4(activeindeces);
+
+
+
+
