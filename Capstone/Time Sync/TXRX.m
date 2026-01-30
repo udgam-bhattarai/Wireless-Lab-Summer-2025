7a518535e5974288be5890bf5a60cab0f2d700d7
AndrewS <127096469+OrangeAah@users.noreply.github.com>
2026-01-30 15:52:57 +0400

    Added Time Sync

---
+++ Capstone/Time Sync/TXRX.m
@@ -0,0 +1,103 @@
+
+clc;
+clear all;
+usrp = findsdru;
+
+%% Variables
+nrb = 20;
+scs = 15;
+ncellid = 42;
+ibar_SSB = 0;
+carrier = nrCarrierConfig('NSizeGrid',nrb,'SubcarrierSpacing',scs);
+dmrsSym = nrPBCHDMRS(ncellid,ibar_SSB);
+dmrsInd = nrPBCHDMRSIndices(ncellid);
+
+refGrid = nrResourceGrid(carrier, 1);
+refGrid(dmrsInd) = dmrsSym;
+refWaveform = nrOFDMModulate(carrier, refGrid);
+
+
+info = nrOFDMInfo(carrier);
+sampleRate = info.SampleRate;
+
+
+%% Defining RX1
+fiveG_rxRadio = comm.SDRuReceiver( ...
+    'Platform',        usrp(1).Platform, ...
+    'SerialNum',       usrp(1).SerialNum, ...
+    'ChannelMapping',  1, ...
+    'CenterFrequency', 2.4e9, ...
+    'Gain',            60, ...
+    'MasterClockRate', 30.72e6, ...   % ↓ halve clock
+    'DecimationFactor',30.72e6 / sampleRate, ...          % keep math simple
+    'SamplesPerFrame', length(refWaveform) * 50, ...
+    'OutputDataType', 'single');
+
+%% Defining RX2
+wifi_rxRadio = comm.SDRuReceiver( ...
+    'Platform',        usrp(2).Platform, ...
+    'SerialNum',       usrp(2).SerialNum, ...
+    'ChannelMapping',  1, ...
+    'CenterFrequency', 2.4e9, ...
+    'Gain',            60, ...
+    'MasterClockRate', 30.72e6, ...   % ↓ halve clock
+    'DecimationFactor',30.72e6 / sampleRate, ...          % keep math simple
+    'SamplesPerFrame', length(refWaveform) * 50, ...
+    'OutputDataType', 'single');
+figure(1);
+%% Capture and Process 5G CSI data
+
+fiveG_rxRadio.ClockSource = 'External';
+% fiveG_rxRadio.PPSSource   = 'External';
+
+wifi_rxRadio.ClockSource = 'External';
+% wifi_rxRadio.PPSSource   = 'External';
+
+
+fiveG_rxRadio.EnableTimeTrigger = true;
+wifi_rxRadio.EnableTimeTrigger = true;
+
+usrpTriggerTime = 10;   % future time in seconds
+fiveG_rxRadio.TriggerTime = usrpTriggerTime;
+wifi_rxRadio.TriggerTime = usrpTriggerTime;
+
+
+while true
+    disp('Capturing 5G...');
+    rxBuf1 = fiveG_rxRadio();
+
+    disp('Capturing Wifi...');
+    rxBuf2 = wifi_rxRadio();
+  
+
+    [H1, valid1] = processNR(rxBuf1, carrier, refGrid, refWaveform, dmrsInd, dmrsSym);
+    [H2, valid2] = processNR(rxBuf2, carrier, refGrid, refWaveform, dmrsInd, dmrsSym);
+
+   % --- Visualization ---
+
+    clf;
+    subplot(2,1,1);
+    if valid1
+        plot(20*log10(abs(H1(:,1))));
+        ylim([-10 30]);
+        title('5GHz Channel Estimate');
+        xlabel('Subcarrier'); ylabel('Magnitude');
+    else
+        title('5GHz: Signal Not Found');
+    end
+    grid on;
+
+    subplot(2,1,2);
+    if valid2
+        plot(20*log10(abs(H2(:,1))));
+        ylim([-10 30]);
+        title('2.4Ghz Channel Estimate');
+        xlabel('Subcarrier'); ylabel('Magnitude');
+        grid on;
+    else
+        title('2.4GHz: Signal Not Found');
+    end
+    grid on;
+
+    drawnow;
+end
