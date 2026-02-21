667f1c0c716fae82432aa1616516a90f71699b00
AndrewS <127096469+OrangeAah@users.noreply.github.com>
2026-02-21 17:21:05 +0400

    testing

--- Capstone/End-to-End/WiFi_5G_CSI.m
+++ Capstone/End-to-End/WiFi_5G_CSI.m
@@ -15,7 +15,6 @@ refGrid = nrResourceGrid(carrier, 1);
 refGrid(dmrsInd) = dmrsSym;
 refWaveform = nrOFDMModulate(carrier, refGrid);
 
-
 info = nrOFDMInfo(carrier);
 sampleRate = info.SampleRate;
 
@@ -28,7 +27,7 @@ fiveG_rxRadio = comm.SDRuReceiver( ...
     'SerialNum',       usrp(1).SerialNum, ...
     'ChannelMapping',  1, ...
     'CenterFrequency', 5e9, ...
-    'Gain',            60, ...
+    'Gain',            70, ...
     'MasterClockRate', 30.72e6, ...   % ↓ halve clock
     'DecimationFactor',30.72e6 / sampleRate, ...          % keep math simple
     'SamplesPerFrame', length(refWaveform) * 50, ...
@@ -47,30 +46,34 @@ wifi_rxRadio = comm.SDRuReceiver( ...
     'OutputDataType', 'single');
 figure(1); 
 % Capture and Process 5G CSI data
+
+testSSID = "TEST_BEACON";
+%% Capture and Process 5G CSI data
+
 while true
     disp('Capturing 5G...');
     rxBuf1 = fiveG_rxRadio();
 
     disp('Capturing Wi-Fi...');
-    % rxBuf2 = wifi_rxRadio();
+    rxBuf2 = wifi_rxRadio();
 
     % Process both buffers
     [H1, valid1] = processNR(rxBuf1, carrier, refGrid, refWaveform, dmrsInd, dmrsSym);
+
     [H2, valid2] = processWiFi(rxBuf2, wifi_cfgNonHT);
- 
+
     % Visualization
     
     clf;
     subplot(2,1,1);
-      % ylim([-50 30]);
+      ylim([-50 30]);
     if valid1
         H1_pilots=zeros(size(H1));
         H1_pilots(dmrsInd) = H1(dmrsInd);
         dmrsRows = 3:4:size(H1_pilots, 1);
-        stem(dmrsRows, 20*log10(abs(H1_pilots(dmrsRows, 2))));
+        plot(dmrsRows, 20*log10(abs(H1_pilots(dmrsRows, 2))));
         title('5G Channel Estimate');
         xlabel('Subcarrier'); ylabel('Magnitude');
-        % ylim([0 50]);
         grid on;
     else
         title('5G: Signal Not Found');
@@ -80,9 +83,8 @@ while true
 
     if valid2
         plot(20*log10(abs(H2)));
-        title('WiFi Channel Estimate');
+        title("WiFi Channel Estimate, Beacon: " + wifiSSID);
         xlabel('Subcarrier'); ylabel('Magnitude');
-        ylim([-50 30]);
         grid on;
     else
         title('WiFi: Signal Not Found');
